a7c8d60487adc91df7832b3a36e3a65e44ff5ae4
WWP1130 <1076506138@qq.com>
2017-12-23 10:24:36 +0800

    commit 的tag

--- wechat_program/src/main/java/com/lendbook/wechat_program/controller/UserLendBook.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/controller/UserLendBook.java
@@ -3,10 +3,14 @@ package com.lendbook.wechat_program.controller;
 import com.lendbook.wechat_program.model.Book;
 import com.lendbook.wechat_program.repository.BookRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.expression.spel.ast.NullLiteral;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.constraints.Null;
+import java.io.EOFException;
+import java.util.*;
 
 @RestController
 public class UserLendBook {
@@ -16,6 +20,71 @@ public class UserLendBook {
 
     @GetMapping(value = "/book/all")
     public List<Book> getAllBook(){
-        return bookRepo.findAll();
+      return bookRepo.findAll();
     }
+
+    @GetMapping(value = "/book/about/{isbn}")
+    public Book findByIsbn13(@PathVariable("isbn") String isbn13)
+    {
+        return bookRepo.findByIsbn13(isbn13);
+    }
+
+   @GetMapping(value = "/book/newbook/{num}" )
+    public List<Book> getNewBook(@PathVariable("num") Integer num)
+    {
+         return  bookRepo.findNewBook(num);
+    }
+
+   @GetMapping(value = "/book/oldbook/{num}" )
+    public List<Book>  getOldBook(@PathVariable("num")Integer num)
+    {
+        return  bookRepo.findOldBook(num);
+    }
+
+   @PostMapping(value = "/book/lend/{isbn}")
+    public Map<String,String> lendBook(@PathVariable("isbn") String isbn13)
+   {
+       Map<String,String> res = new HashMap<String, String>();
+
+        if(bookRepo.findByIsbn13(isbn13)==null)
+       {
+           res.put("result","未找到此书");
+       }
+       else {
+           Book book = bookRepo.findByIsbn13(isbn13);
+           int num1 = book.getStoreNum();
+           int num2 = book.getLendNum();
+           if (num1 > 0) {
+               num1 = num1 - 1;
+               num2 = num2 + 1;
+               book.setStoreNum(num1);
+               book.setLendNum(num2);
+               bookRepo.save(book);
+               res.put("result", "成功");
+           } else {
+               res.put("result", "失败");
+               System.out.print("库存不足");
+           }
+       }
+       return  res;
+   }
+
+    @PostMapping(value = "book/search")
+    public Book searchBook(@RequestParam("searchStr") String searchStr)
+    {
+        return bookRepo.findSearchStr(searchStr);
+    }
+
+
+
+    // @GetMapping(value = "book/id/{id}")
+
+    //@GetMapping(value = "book/isbn/{isbn}")
+
+    //发送邮件
+
+    //确认邮件
+
+   // @GetMapping(value = "/tags/all/get")
+
 }
--- wechat_program/src/main/java/com/lendbook/wechat_program/repository/BookRepo.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/repository/BookRepo.java
@@ -2,7 +2,26 @@ package com.lendbook.wechat_program.repository;
 
 import com.lendbook.wechat_program.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookRepo  extends JpaRepository<Book, Integer> {
-    Book findByIsbn13 (String isbn13);
+
+
+    //通过ISBN号查询
+    public Book findByIsbn13(String isbn13);
+
+    //查询前n本新书
+    @Query(value = "SELECT * FROM  Book  ORDER  BY  publish_date DESC  limit ?1",nativeQuery = true )
+    List<Book> findNewBook(Integer num);
+
+    //查询旧书且好评率高的前n本书
+    @Query(value = "SELECT * FROM Book WHERE distinc_old_or_new = TRUE ORDER BY rating DESC limit ?1 ",nativeQuery = true)
+    List<Book> findOldBook(Integer num);
+
+    //根据isbn号或者作者查书
+    @Query(value = "SELECT * FROM Book WHERE isbn13=?1 OR  author[0] LIKE ?1% OR  author[0] LIKE ?1%   ",nativeQuery = true)
+     Book findSearchStr(String searchStr);
 }
