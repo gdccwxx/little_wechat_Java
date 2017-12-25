d715ff53ff554b00c39b7695dea190da9cf085d7
gdccwxx <765553928@qq.com>
2017-12-26 01:13:02 +0800

    add some feature

--- wechat_program/src/main/java/com/lendbook/wechat_program/model/LendBook.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/model/LendBook.java
@@ -1,14 +1,68 @@
 package com.lendbook.wechat_program.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Date;
+import java.util.Calendar;
 @Entity
 public class LendBook {
     @Id
+    @GeneratedValue
+    private Integer id;
     private String wechat;
     private String isbn;
-    private Date lendTime;
-    private Date returnTime;
+    private Calendar lendTime;
+    private Calendar returnTime;
     private Boolean distincReturn;
+
+    public LendBook() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Calendar getLendTime() {
+        return lendTime;
+    }
+
+    public void setLendTime(Calendar lendTime) {
+        this.lendTime = lendTime;
+    }
+
+    public Calendar getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(Calendar returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public Boolean getDistincReturn() {
+        return distincReturn;
+    }
+
+    public void setDistincReturn(Boolean distincReturn) {
+        this.distincReturn = distincReturn;
+    }
 }
--- wechat_program/src/main/java/com/lendbook/wechat_program/model/User.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/model/User.java
@@ -8,9 +8,54 @@ import javax.validation.constraints.NotNull;
 @Entity
 public class User {
     @Id
+    @GeneratedValue
+    private Integer id;
     @NotNull
     private String wechat;
     private String email;
     private String vircode;
     private long createVirTime;     // create vircode timestramp;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVircode() {
+        return vircode;
+    }
+
+    public void setVircode(String vircode) {
+        this.vircode = vircode;
+    }
+
+    public long getCreateVirTime() {
+        return createVirTime;
+    }
+
+    public void setCreateVirTime(long createVirTime) {
+        this.createVirTime = createVirTime;
+    }
 }
