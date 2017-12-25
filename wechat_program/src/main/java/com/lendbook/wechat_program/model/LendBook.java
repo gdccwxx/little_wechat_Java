33eb24da5878011c580b98ef39c1cb8f69a34cc6
WWP1130 <1076506138@qq.com>
2017-12-25 17:11:32 +0800

    lendbook

--- wechat_program/src/main/java/com/lendbook/wechat_program/model/LendBook.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/model/LendBook.java
@@ -2,13 +2,34 @@ package com.lendbook.wechat_program.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Calendar;
 import java.util.Date;
 @Entity
 public class LendBook {
     @Id
     private String wechat;
     private String isbn;
-    private Date lendTime;
-    private Date returnTime;
+    private Calendar lendTime;
+    private Calendar returnTime;
     private Boolean distincReturn;
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setLendTime(Calendar lendTime) {
+        this.lendTime = lendTime;
+    }
+
+    public void setReturnTime(Calendar returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public void setDistincReturn(Boolean distincReturn) {
+        this.distincReturn = distincReturn;
+    }
 }
