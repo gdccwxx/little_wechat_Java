4d4388f7804c7305ae461bdcd3ec4f7bb5cb279a
gdccwxx <765553928@qq.com>
2017-12-28 01:58:18 +0800

    what the fuck???

--- wechat_program/src/main/java/com/lendbook/wechat_program/Tools/SendMail.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/Tools/SendMail.java
@@ -1,60 +1,40 @@
 package com.lendbook.wechat_program.Tools;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
-import java.security.Security;
-import java.util.Properties;
 
+@Service
 public class SendMail {
+    @Autowired
+    private JavaMailSender sender;
+    @Value("${spring.mail.username}")
+    private String from;
 
-    public Boolean sendHtmlMail(String title, ) {
-        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-        // Get a Properties object
-        Properties props = new Properties();
-        props.setProperty("mail.smtp.host", "smtp.126.com");
-        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-        props.setProperty("mail.smtp.socketFactory.fallback", "false");
-        props.setProperty("mail.smtp.port", "465");
-        props.setProperty("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.auth", "true");
-        final String username = "lcdxlh@126.com";
-        final String password = "lc1997712";
-        Session session = Session.getDefaultInstance(props, new Authenticator(){
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }});
-        session.setDebug(true);
-        // -- Create a new message --
-        Message msg = new MimeMessage(session);
-        // -- Set the FROM and TO fields --
-        String nick="设计甲鱼红豆";
+    public void sendHtmlMail(String to, String subject, String content){
+
+        MimeMessage message = sender.createMimeMessage();
+
         try {
-            nick=javax.mail.internet.MimeUtility.encodeText(nick);
-        } catch (UnsupportedEncodingException e) {
+            MimeMessageHelper helper = setInfoByHelper(to, subject, content, message);
+
+            sender.send(message);
+        } catch (MessagingException e) {
             e.printStackTrace();
         }
-        msg.setFrom(new InternetAddress("gdccwxx@163.com"));
-        msg.setRecipients(Message.RecipientType.TO,
-                InternetAddress.parse(to,false));
-        msg.setSubject(title);
-        msg.setContent(content,"text/html; charset=utf-8");
-        msg.setSentDate(new Date());
-        Transport.send(msg);
-
-        System.out.println("Message sent.");
-        return true;
+    }
+    private MimeMessageHelper setInfoByHelper(String to, String subject, String content, MimeMessage message)
+            throws MessagingException {
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        return helper;
     }
 }
--- wechat_program/src/main/java/com/lendbook/wechat_program/controller/testing.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/controller/testing.java
@@ -1,14 +1,21 @@
 package com.lendbook.wechat_program.controller;
 
 import com.lendbook.wechat_program.Tools.SendMail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class testing {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    private String to = "765553928@qq.com";
     @GetMapping(value = "/testing")
-    public void test(){
-        SendMail sendMail = new SendMail();
-        sendMail.sendHtmlMail();
+    public void sendEmail()  {
+        new SendMail().sendHtmlMail(to, "主题：html邮件", "<h1>today is perfect day right???</h1><h2>i don't konw why it is 554</h2>");
     }
 }
