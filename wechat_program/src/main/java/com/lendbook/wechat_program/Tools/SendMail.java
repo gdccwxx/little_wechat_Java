ed52c94d73168b8c2de786a389cf1fe1b14340bc
gdccwxx <765553928@qq.com>
2017-12-28 19:16:36 +0800

    email finish

--- wechat_program/src/main/java/com/lendbook/wechat_program/Tools/SendMail.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/Tools/SendMail.java
@@ -1,25 +1,29 @@
 package com.lendbook.wechat_program.Tools;
 
+import com.lendbook.wechat_program.component.EmailProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 @Service
 public class SendMail {
-    private String from = "gdccwxx@163.com";
+    @Autowired
+    private EmailProperties emailProperties;
 
-    public void sendHtmlMail(String to, String subject, String content, JavaMailSender sender){
+    @Autowired
+    private  JavaMailSender sender;
+    public void sendHtmlMail(String to, String subject, String content){
 
         MimeMessage message = sender.createMimeMessage();
 
         try {
             MimeMessageHelper helper = setInfoByHelper(to, subject, content, message);
-
+            message.addRecipients(Message.RecipientType.CC, emailProperties.getUsername());
             sender.send(message);
         } catch (MessagingException e) {
             e.printStackTrace();
@@ -28,7 +32,7 @@ public class SendMail {
     private MimeMessageHelper setInfoByHelper(String to, String subject, String content, MimeMessage message)
             throws MessagingException {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setFrom(from);
+        helper.setFrom(emailProperties.getUsername());
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(content, true);
--- wechat_program/src/main/java/com/lendbook/wechat_program/controller/testing.java
+++ wechat_program/src/main/java/com/lendbook/wechat_program/controller/testing.java
@@ -3,19 +3,22 @@ package com.lendbook.wechat_program.controller;
 import com.lendbook.wechat_program.Tools.SendMail;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
 public class testing {
     @Autowired
-    private JavaMailSender javaMailSender;
-
-    private String to = "765553928@qq.com";
-    @GetMapping(value = "/testing")
-    public void sendEmail()  {
-        new SendMail().sendHtmlMail(to, "主题：html邮件", "<h1>today is perfect day right???</h1><h2>i don't konw why it is 554</h2>",javaMailSender);
+    private SendMail sendMail;
+    @PostMapping(value = "/testing")
+    public void sendEmail(@RequestParam("to") String to,@RequestParam("title") String title, @RequestParam("content") String content)  {
+        try{
+            sendMail.sendHtmlMail(to, title, content);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
